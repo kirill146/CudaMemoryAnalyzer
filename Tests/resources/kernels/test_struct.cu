a7514ace3cf2c198c6b4df3e5ec3b3ad803ce57d
Kirill Kozlov <drxu59@gmail.com>
2021-06-05 12:36:59 +0500

    Minor update of test_struct

--- Tests/resources/kernels/test_struct.cu
+++ Tests/resources/kernels/test_struct.cu
@@ -3,17 +3,17 @@ __global__ void test_uchar4(uchar4* const c)
 	int a[5];
 	uchar4 val;
 	val.x = 10;
-	a[val.x];
+	a[val.x] = 42;
 	uchar4 val2 = val;
-	a[val2.x];
+	a[val2.x] = 42;
 	uchar4 val3;
 	val3 = val;
-	a[val3.x];
+	a[val3.x] = 42;
 	uchar4 val4[3];
 	val4[1] = val;
-	a[val4[1].x];
+	a[val4[1].x] = 42;
 	c[1].y = 9;
 	c[1].w = 3;
 	int val5 = c[1].y;
-	a[val5];
+	a[val5] = 42;
 }
